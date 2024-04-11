a280825d851604f0e137c9b4af12b41150d2161a
Arbaj_1147 <shaikharbaj2001@gmail.com>
2024-04-11 15:41:50 +0530

    add loader

--- src/app/Components/Loader/Loader.js
+++ src/app/Components/Loader/Loader.js
@@ -1,9 +1,13 @@
-import React from 'react'
-import styles from './loader.module.css'
+import React from "react";
+import styles from "./loader.module.css";
 const Loader = () => {
   return (
-    <div className={styles.loader}>Loader</div>
-  )
-}
+    <div className={styles.loader}>
+      <div className="spinner-border" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  );
+};
 
 export default Loader;
